74a17952352bed588c833469fe5172e330264bb7
yurii macbook pro <yurikorock777@gmail.com>
2025-09-09 18:04:02 +0300

    add styles to header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Layout from "./Layout/Layout.jsx";
 import HomePage from "../pages/HomePage/HomePage.jsx";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.jsx";
-import AppHeader from "./AppHeader/AppHeader.jsx";
 import CatalogPage from "../pages/CatalogPage/CatalogPage.jsx";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.jsx";
+
 
 export default function App() {
   return (
     <div>
-      <AppHeader/>
+     
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<HomePage />} />
+          <Route path="catalog" element={<CatalogPage />} />
+          {/* <Route path="catalog:id" element={<ElementPage/>}/> */}
           <Route path="*" element={<NotFoundPage />} />
+          </Route>
         </Routes>
      
     </div>
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,22 @@
+import { NavLink, Outlet } from "react-router-dom";
+import css from "./Layout.module.css"
+
+export default function Layout() {
+    const setActive = ({isActive}) => isActive ? css.activeLink : css.noActiveLink;
+  return (
+    <div className="container">
+      <header className={css.header}>
+        <NavLink to="/" className={css.logo}>
+          Rental<span>Car</span>
+        </NavLink>
+        <nav className={css.nav}>
+          <NavLink to="/" className={setActive}>Home</NavLink>
+          <NavLink to="/catalog" className={setActive}>Catalog</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  );
+}
