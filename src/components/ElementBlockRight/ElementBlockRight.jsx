b7f38a008561520119c9ffcb13f74b3de6714c62
yurii macbook pro <yurikorock777@gmail.com>
2025-09-13 19:18:58 +0300

    add top-block includes namecar

--- src/components/ElementBlockRight/ElementBlockRight.jsx
+++ src/components/ElementBlockRight/ElementBlockRight.jsx
@@ -1,4 +1,6 @@
 import css from "./ElementBlockRight.module.css";
+import sprite from "../../../public/sprite.svg"
+import updateAddress from "../../helpers/helpers.js";
 
 export default function ElementBlockRight({ car }) {
   const {
@@ -19,11 +21,24 @@ export default function ElementBlockRight({ car }) {
   } = car;
   return (
     <div className={css.right_block}>
-      <div className={css.name_block}>
-        <h2>
-          {brand} {model}, {year}
-        </h2>
-        <p>Id: {id.slice(0, 4)}</p>
+      <div className={css.top_block}>
+          <div className={css.name_block}>
+            <h2 className={css.name}>
+              {brand} {model}, {year}
+            </h2>
+            <p className={css.id}>Id: {id.slice(0, 4)}</p>
+          </div>
+          <div className={css.address_box}>
+            <div className={css.address_box_item}>
+                <svg className={css.icon_location}>
+                    <use href={sprite + "#icon-location"}/>
+                </svg>
+                 <p className={css.address}>{updateAddress(address)}</p>
+            </div>
+            <p className={css.mileage}>Mileage: {mileage.toLocaleString()} km</p>
+          </div>
+          <p className={css.price}>$ {rentalPrice}</p>
+          <p className={css.description}>{description}</p>
       </div>
     </div>
   );
